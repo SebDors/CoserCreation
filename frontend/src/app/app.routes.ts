a8c334f514d48efbd8da6d92147df85dcaa32d62
margaultmg <margault.mons-gouillaud@epfedu.fr>
2026-01-19 14:02:45 +0100

    INCROYABLEEEE

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: GalleryComponent },
+  { path: '', component: HomeComponent },
+  { path: 'galerie', component: GalleryComponent },
   { path: 'contact', component: ContactFormComponent },
   { path: 'product/:id', component: ProductDetailComponent },
   {
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
-import { GalleryComponent } from '../gallery/gallery.component';
-import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
-import { ContactFormComponent } from '../contact-form/contact-form.component';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Product } from '../product.model';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [GalleryComponent, NewsletterFormComponent, ContactFormComponent],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  featuredProducts$!: Observable<Product[]>;
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.featuredProducts$ = this.dataService.getProducts().pipe(
+      map(products => products.slice(0, 3))
+    );
+  }
 }
