700b3c74c988d5d6555181eafce4d9e20da9e729
Sebastien Dors <dorssebastien@gmail.com>
2026-01-28 21:45:55 +0100

    DELETE

--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { authGuard } from '../auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] }
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
